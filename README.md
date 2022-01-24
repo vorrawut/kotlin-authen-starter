65eae7f7c4fc282dde0c18a7ba4f8a8bdb484674
Vorrawut Judasri <vorrawutjud@gmail.com>
2022-01-24 21:26:16 +0700

    [wut][Swagger] intial swagger

---
+++ README.md
@@ -0,0 +1,2 @@
+#Swagger
+url: http://localhost:8081/swagger-ui.html
--- build.gradle.kts
+++ build.gradle.kts
@@ -36,11 +36,15 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 
+
 //	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 //	implementation("org.springframework.cloud:spring-cloud-gcp-starter")
 
 	//	Bigtable
 	implementation("com.google.cloud:google-cloud-bigtable:2.5.1")
+
+	implementation("io.springfox:springfox-swagger2:2.9.2")
+	implementation("io.springfox:springfox-swagger-ui:2.9.2")
 
 	compileOnly("org.projectlombok:lombok")
 	annotationProcessor("org.projectlombok:lombok")
